9b73bc617674aad7d9d7212c819e05765fe6cd10
Dmitry Koshelev <d.koshelev@ltst.ru>
2023-06-24 19:06:18 +0600

    Fix changing model by swipe

--- GressoApp/GressoApp/ARFittingRoomView.swift
+++ GressoApp/GressoApp/ARFittingRoomView.swift
@@ -6,30 +6,39 @@
 //
 
 import SwiftUI
-
-class ModelEntityProvider: ObservableObject {
-    
-    @Published var modelDestination: URL? = nil
-    
-    func changeDestination(destination: URL) {
-        modelDestination = destination
-    }
-}
-
+import RealityKit
+ 
 struct ARFittingRoomView: View {
+    @Environment(\.dismiss) var dismiss
     
     let destinations: [URL]
     
     @State private var tabIndex: Int = .zero
+    @State private var currentDestination: URL?
     
     var body: some View {
-        let arView = ARViewContainer(destinations: destinations, currentIndex: $tabIndex)
+        let arView = ARViewContainer(
+            destinations: destinations,
+            currentIndex: $tabIndex,
+            currentDestination: Binding(
+                get: {
+                    destinations[tabIndex]
+                }, set: { _ in }
+            )
+        )
+        
         ZStack {
             arView
-            
+            VStack {
+                Button("dismiss") {
+                    dismiss()
+                }
+                .padding()
+                Spacer()
+            }
             VStack {
                 Spacer()
-                
+
                 TabView(selection: $tabIndex) {
                     ForEach(Array(zip(destinations.indices, destinations)), id: \.0) { index, destination in
                         Text(destination.lastPathComponent)
@@ -42,11 +51,5 @@ struct ARFittingRoomView: View {
             }
         }
         .ignoresSafeArea(.all)
-    }
-}
-
-struct ARFittingRoomView_Previews: PreviewProvider {
-    static var previews: some View {
-        ARFittingRoomView(destinations: [])
     }
 }
--- GressoApp/GressoApp/ARViewContainer.swift
+++ GressoApp/GressoApp/ARViewContainer.swift
@@ -11,21 +11,16 @@ import ARKit
 
 struct ARViewContainer: UIViewRepresentable {
     
-    private let arView = ARView(frame: .zero)
-    
     let destinations: [URL]
     @Binding var currentIndex: Int
-    @State private var currentDestination: URL?
+    @Binding var currentDestination: URL?
     
     func makeUIView(context: Context) -> ARView {
+        let arView = ARView(frame: .zero)
         arView.renderOptions = [.disablePersonOcclusion]
         
         let faceTrackingConfig = ARFaceTrackingConfiguration()
         arView.session.run(faceTrackingConfig, options: [.resetTracking, .removeExistingAnchors])
-        
-        let tap = UITapGestureRecognizer(target: context.coordinator,
-                                         action: #selector(context.coordinator.handleTap(_:)))
-        arView.addGestureRecognizer(tap)
         
         return arView
     }
@@ -42,22 +37,6 @@ struct ARViewContainer: UIViewRepresentable {
             } catch {
                 print("Fail loading entity.", error.localizedDescription)
             }
-        }
-    }
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-    
-    class Coordinator: NSObject, UIGestureRecognizerDelegate {
-        var parent: ARViewContainer
-        
-        init(_ parent: ARViewContainer) {
-            self.parent = parent
-        }
-        
-        @objc func handleTap(_ sender: UITapGestureRecognizer) {
-            parent.currentDestination = parent.destinations[parent.currentIndex]
         }
     }
     
